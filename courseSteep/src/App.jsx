285ff3caba2c8f3794018885cc0da48894f24640
AppSiDaL <gilielcrack343@gmail.com>
2023-08-14 09:52:55 -0600

    deploying app

--- courseSteep/src/App.jsx
+++ courseSteep/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("some error happened...");
+  const [errorMessage, setErrorMessage] = useState(null);
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
