9fe611048381a3e26b2b253ca51f8f58c37a0091
AppSiDaL <giliecrack343@gmail.com>
2023-10-14 07:43:58 -0600

    fixed eslint

--- part11/full-stack-open-pokedex/cypress/e2e/spec.cy.js
+++ part11/full-stack-open-pokedex/cypress/e2e/spec.cy.js
@@ -1,7 +1,10 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
+    /* eslint-disable no-undef */
     cy.visit('http://localhost:8080')
+    /* eslint-disable no-undef */
     cy.contains('ivysaur')
+    /* eslint-disable no-undef */
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
